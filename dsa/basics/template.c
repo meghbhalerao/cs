4e3edd3e223533991f0324755b14fba36ae2187b
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-03-24 21:26:59 +0530

    added some other functions

--- dsa/basics/template.c
+++ dsa/basics/template.c
@@ -1,1 +1,29 @@
 #include<stdio.h>
+
+struct Rectangle
+{
+int l;
+int b;
+}
+
+void initialize(struct Rectangle *r, int le, int br)
+{
+(*r).l = le;
+(*r).b = br;
+}
+int area(struct Rectangle r)
+{
+return r.l*r.b
+}
+void changeLength(struct Rectangle *r, int nl)
+{
+r->l = nl
+}
+
+
+
+}
+int main()
+{
+printf("Hello");
+}
