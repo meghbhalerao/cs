f82fff9d6ee4285f110d513ba3d31efe4ddca219
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-03 15:05:28 +0530

    cpp class for rectangle added

--- dsa/basics/rect.cpp
+++ dsa/basics/rect.cpp
@@ -1,4 +1,5 @@
 #include<iostream.h>
+using namespace std
 class Rectangle
 {
 	private:
@@ -13,4 +14,29 @@ class Rectangle
 		void setLength(int l){length =l}
 		~Rectangle();
 };
+Rectangle :: Rectangle(int l,int b)
+{
+length = l;
+breadth = b;
+}
+int Rectangle :: area()
+{
+return length*breadth;
+}
+int Rectangle :: Rectangle()
+{
+return 2*(length+breadth)
+}
+Rectangle :: ~Rectangle()
+{
+}
+int main()
+{
+Rectangle r(10,5);
+cout<<r.area();
+cout<<r.perimeter();
+r.setLength(20);
+cout<<r.getLength()
+// Here the destructor is called automatically to destroy the object since here we are going outside the scope of the object, since the object is not accessible outside the main function
+}
 
