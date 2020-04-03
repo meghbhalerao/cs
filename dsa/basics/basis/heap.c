65b0e8c080979a6b67cda334418a085e11419a06
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-03 16:46:13 +0530

    somr uncommitted changes

---
+++ dsa/basics/basis/heap.c
@@ -0,0 +1,13 @@
+void main()
+{
+// memory to the pointer is allocated in the stack memory
+int *p;
+// array of size 5 in heap memory is allocated 
+p = new int[5]
+// syntax in c language - above syntax in cpp language
+p = (int*)malloc(sizeof(int)*5)
+// when we do not need the heap memory that we need to first deallocate and the make the pointer as null
+delete []p
+// if delete is not used then the heap memory is simplt lost and hence first must be deleted
+p = NULL;
+}
