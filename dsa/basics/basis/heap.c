2e4b8fe40b3e0b68c522d7c57af216c381e4b9dd
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-03 19:00:09 +0530

    some uncommitted changes

--- dsa/basics/basis/heap.c
+++ dsa/basics/basis/heap.c
@@ -9,5 +9,7 @@ p = (int*)malloc(sizeof(int)*5)
 // when we do not need the heap memory that we need to first deallocate and the make the pointer as null
 delete []p
 // if delete is not used then the heap memory is simplt lost and hence first must be deleted
+// loss of memory is called memory leak
+// heap memory allocation done with the help of pointers and cannot be allocated directly
 p = NULL;
 }
