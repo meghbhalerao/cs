5d77c6e2192482f5bcf509121bd684be4fdbb6f4
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-06 17:02:09 +0530

    some more ex

--- dsa/recursion/problems/combination.c
+++ dsa/recursion/problems/combination.c
@@ -2,4 +2,22 @@
 // Pascals traiangle for getting coefficients of a binomial expansion 
 // Pascals triangle can be obtained by recursive addition of using pascals traingles
 // Pascals traingle is a beautiful example of recursion - also is in the tree format - the method how all recursive functions are traced
+#include<stdio.h>
+int nCr(int n, int r)
+{
+    if(r==0||n==r)
+    {
+        return 1;
+    }
+    else
+    {
+        return nCr(n-1,r) + nCr(n-1,r-1);
+    }
+    
 
+}
+int main()
+{
+    printf("%d",nCr(6,3));
+    return 0;
+}
---
+++ dsa/recursion/problems/hanoi.c
@@ -0,0 +1,16 @@
+// larger disk shoud not be above smaller disk - B is like an auxilliary tower - O(2^n)
+#include<stdio.h>
+void TOH(int n, int A, int B, int C)
+{
+    if(n>0)
+    {
+        TOH(n-1,A,C,B);
+        printf("(%d,%d)\n",A,C);
+        TOH(n-1,B,A,C);
+    }
+}
+int main()
+{
+    TOH(3,1,2,3);
+    return 0;
+}
