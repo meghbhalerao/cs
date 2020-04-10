96b00b0a3335d562059979564ab0b175e5402807
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-10 17:29:12 +0530

    some more arrays

---
+++ dsa/array/arr3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int A[3][4] = {{1,2,3,4},{2,4,6,8},{1,3,5,7}};
+    int *B[3];
+    B[0] = (int*)malloc(4*sizeof(int));
+    B[1] = (int*)malloc(4*sizeof(int));
+    B[2] = (int*)malloc(4*sizeof(int));
+
+    C = (int**)malloc(3*sizeof(int*));
+    
+
+
+
+    return 0;
+
+}
