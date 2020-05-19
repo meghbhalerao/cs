8e39627a8476f3864ad2f75d73b6f365d58b0ffd
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-05-19 19:12:57 +0530

    added linked list

--- dsa/linked-list/display.c
+++ dsa/linked-list/display.c
@@ -10,7 +10,7 @@ struct Node
 }* first = NULL;
 
 // Function for creating a linked list
-void create(int A[], int n)
+void Create(int A[], int n)
 {
     struct Node *t, *last;
     first = (struct Node*)malloc(sizeof(struct Node));
@@ -18,22 +18,32 @@ void create(int A[], int n)
     first->next = NULL;
     last = first;
     for(int i=1;i<n;i++)
-    {     
-        t = 
-        last = last->next;
-        last->data = A[i];
+    {    
+        // now we want to create a new node temp - hence we make use of a varible t - to create a temporary node and then assign that node to be the last node 
+        t = (struct Node*)malloc(sizeof(struct Node));
+        t->data =  A[i];
+        // since as of now this is the last node in the linked list - since as of now nothing mode has been added after this - hence we set the next value as NULL in this temp variable
+        t->next = NULL;
+        // assigning the previous node's address column tp the last node's address
+        last->next = t;
+        // setting the last node as the node which is added latest i.e. t
+        last = t;
+
     }
-
-
 
 }
 
+// Displaying the linked list
 
 
 
 int main()
 {
     int A[] = {3,5,7,10,15};
+
+    Create(A,5);
+
+
     return 0;
 }
 
